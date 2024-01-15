8a93446e7f3ee9ed81b61a7c990aa054e865ad63
Rdrg974 <hassany.rodrigue@gmail.com>
2024-01-15 05:31:57 -0800

    All the prototype and the struct

--- sort.h
+++ sort.h
@@ -1,6 +1,10 @@
 #ifndef SORT_H
 #define SORT_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 /**
  * struct listint_s - Doubly linked list node
  *
@@ -17,5 +21,9 @@ typedef struct listint_s
 
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
+void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
 
 #endif
